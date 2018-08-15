e88c9f6d3f99c7f1253903edd81f62925040cce8
german <germanrobledo@gmail.com>
2018-08-15 02:06:33 -0300

    first commit

---
+++ src/main/java/com/kimisoft/cineManager/CineManagerApplication.java
@@ -0,0 +1,12 @@
+package com.kimisoft.cineManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kimisoft/cineManager/CineManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kimisoft.cineManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CineManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
