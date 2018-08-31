65646fc6a7d7050670775a839c77542b6546b29d
german <germanrobledo@gmail.com>
2018-08-31 02:46:51 -0300

    agregue servicio al rest de user

--- src/main/java/com/kimisoft/cineManager/CineManagerApplication.java
+++ src/main/java/com/kimisoft/cineManager/CineManagerApplication.java
@@ -2,6 +2,7 @@ package com.kimisoft.cineManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CineManagerApplication {
@@ -9,4 +10,9 @@ public class CineManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CineManagerApplication.class, args);
 	}
+	
+//	@Bean
+//	 public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//	  return new BCryptPasswordEncoder();
+//	 }
 }
--- src/main/java/com/kimisoft/cineManager/rest/UserRestController.java
+++ src/main/java/com/kimisoft/cineManager/rest/UserRestController.java
@@ -4,18 +4,26 @@ package com.kimisoft.cineManager.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.kimisoft.cineManager.model.User;
 import com.kimisoft.cineManager.service.UserService;
 
 @RestController
 public class UserRestController {
+	
+//	@Autowired
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
 	private UserService userService;
@@ -27,5 +35,32 @@ public class UserRestController {
 			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<List<User>>(resultList, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/user/{id}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getUser(@PathVariable("id") long id){
+		System.out.println("obteniendo user con id=" + id);
+		User user = userService.findById(id);
+		if(user == null){
+			System.out.println("No se encontro user con id " + id);
+			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/user/", method= RequestMethod.POST)
+	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucBuilder){
+		System.out.print("Creando el usuario " + user.getUsername());
+		
+		if (userService.isUserExist(user)){
+			System.out.println("El usuario " + user.getUsername() + " ya existe");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		//encripto las password
+//		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		userService.saveUser(user);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+		return new ResponseEntity<Void>(headers,HttpStatus.CREATED);
 	}
 }
